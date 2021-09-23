d916838ff6566fdbb66ddffaff6e5a5a426a2dc9
Vladimir Berdnikov <gazetter@mail.ru>
2021-09-24 00:31:09 +0700

    Добавьте файлы проекта.

---
+++ CurrencyConverter/ViewModels/CalcViewModel.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CurrencyConverter.ViewModels
+{
+    public class CalcViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public string CharCode1 {get; set; }
+        public string CharCode2 { get; set; }
+        public bool ChangeButton1 { get; set; }
+        public bool ChangeButton2 { get; set; }
+
+        public CalcViewModel()
+        {
+            CharCode1 = "RUB";
+            CharCode2 = "USD";
+            ChangeButton1 = false;
+            ChangeButton2 = false;
+        }
+
+
+
+    }
+}
---
+++ CurrencyConverter/ViewModels/ValutesViewModel.cs
@@ -0,0 +1,26 @@
+﻿using CurrencyConverter.Model;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CurrencyConverter.ViewModels
+{
+    public class ValutesViewModel
+    {
+        private ValuteRepository _valuteRepository { get; }
+
+        public ObservableCollection<Valute> Valutes 
+        {
+            get => new ObservableCollection<Valute>(_valuteRepository.GetAll());
+        }
+
+        public ValutesViewModel()
+        {
+            _valuteRepository = new ValuteRepository();
+        }
+
+    }
+}
